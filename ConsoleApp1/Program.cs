6efbe8423ca94a4e1f26647a7610ed2a4bd24efb
thanh2k1582 <71741876+thanh2k1582@users.noreply.github.com>
2021-12-10 13:41:46 +0700

    bai 6

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QLHS quanLyHocSinh= new QLHS();
+            while (true)
+            {
+                Console.WriteLine("\nCHUONG TRINH QUAN LY KHACH SAN");
+                Console.WriteLine("*************************MENU**************************");
+                Console.WriteLine("**  1. Them hoc sinh.                                **");
+                Console.WriteLine("**  2. Hien thi danh sach hoc sinh.                  **");
+                Console.WriteLine("**  3. Hien thi danh sach hoc sinh 20 tuoi           **");
+                Console.WriteLine("**  4. Hien thi danh sach hoc sinh 23 tuoi va o DN   **");
+                Console.WriteLine("**  0. Thoat                                         **");
+                Console.WriteLine("*******************************************************");
+                Console.Write("Nhap tuy chon: ");
+                int key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 1:
+                        quanLyHocSinh.NhapHS();
+                        Console.WriteLine("\nThem khach hang thanh cong!");
+                        break;
+
+                    case 2:
+                        quanLyHocSinh.XuatHS(quanLyHocSinh.getListHocSinh());
+                        break;
+                    case 3:
+                        quanLyHocSinh.XuatHS20T(quanLyHocSinh.getListHocSinh());
+                        break;
+                    case 4:
+                        quanLyHocSinh.CountHS(quanLyHocSinh.getListHocSinh());
+                        break;
+                    case 0:
+                        Console.WriteLine("\nBan da chon thoat chuong trinh!");
+                        return;
+                    default:
+                        Console.WriteLine("\nKhong co chuc nang nay!");
+                        Console.WriteLine("\nHay chon chuc nang trong hop menu.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/QLHS.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class QLHS
+    {
+        private List<HocSinh> ListHocSinh = null;
+        public QLHS()
+        {
+            ListHocSinh = new List<HocSinh>();
+        }
+        public void NhapHS()
+        {
+            HocSinh hs = new HocSinh();
+            Console.Write("Nhap thong tin hoc sinh: \n");
+            Console.Write("Nhap ten: ");
+            hs.HoTen = Convert.ToString(Console.ReadLine());
+
+            Console.Write("Nhap tuoi: ");
+            hs.Tuoi = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Nhap que quan: ");
+            hs.QueQuan = Convert.ToString(Console.ReadLine());
+            ListHocSinh.Add(hs);
+        }
+        public void XuatHS(List<HocSinh> listHS)
+        {
+            foreach (HocSinh hs in listHS)
+            {
+                Console.WriteLine("************************");
+                Console.WriteLine("Thong tin hoc sinh");
+                Console.WriteLine("Ten: {0}", hs.HoTen);
+                Console.WriteLine("Tuoi: {0}", hs.Tuoi);
+                Console.WriteLine("Que Quan: {0}", hs.QueQuan);
+            }
+            Console.WriteLine();
+        }
+        public void XuatHS20T(List<HocSinh> listHS)
+        {
+            int count = 0;
+            foreach (HocSinh hs in listHS)
+            {
+                if (hs.Tuoi == 20)
+                {
+                    Console.WriteLine("************************");
+                    Console.WriteLine("Thong tin hoc sinh");
+                    Console.WriteLine("Ten: {0}", hs.HoTen);
+                    Console.WriteLine("Tuoi: {0}", hs.Tuoi);
+                    Console.WriteLine("Que Quan: {0}", hs.QueQuan);
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("Khong co hoc sinh nao 20 tuoi");
+            }
+            Console.WriteLine();
+        }
+        public void CountHS(List<HocSinh> listHS)
+        {
+            int count = 0;
+            foreach (HocSinh hs in listHS)
+            {
+                if (hs.Tuoi == 23 && hs.QueQuan.ToUpper() == "DN")
+                {
+                    count++;
+                }
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("Khong co hoc sinh nao 23 tuoi va o DN");
+            }
+            else
+            {
+                Console.WriteLine("Co {0} hoc sinh 23 tuoi va o DN",count);
+            }
+            Console.WriteLine();
+        }
+        public List<HocSinh> getListHocSinh()
+        {
+            return ListHocSinh;
+        }
+    }
+}
